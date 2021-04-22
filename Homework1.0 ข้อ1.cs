3471d66155b09e691094023e552ec3520e4ee98d
asdiop153246 <78408481+asdiop153246@users.noreply.github.com>
2021-04-23 02:19:57 +0700

    ไม่รู้ทำยังไงให้แถว 0 มันเป็นแถว 1 เพราะฉนั้น นอนก่อนละกัน

---
+++ Homework1.0 ข้อ1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Homeowkr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Pascal();
+            
+
+            
+
+        }
+        static void Pascal()
+        {
+            int rows;
+            int value = 1;
+            int space;
+            int i;
+            int j;
+            Console.Write("Input the Pascal row : ");
+            rows = int.Parse(Console.ReadLine());
+            if (rows < 0)
+            {
+                Invalid();
+            }
+
+            while (rows > 0)
+            {
+                for (i = 1; i < rows; i++)
+                {
+                    for (space = 0; space <= rows - i; space++)
+                        Console.Write(" ");
+                    for (j = 0; j <= i; j++)
+                    {
+                        if (j == 0 || i == 0)
+                            value = 1;
+                        else
+                            value = value * (i - j + 1) / j;
+                        Console.Write(value + " ");
+                    }
+                    Console.WriteLine();
+                    
+                }
+                break;
+            }
+        }
+        static void Invalid()
+        {
+            Console.WriteLine("Invalid Pascal Triangle");
+            Pascal();
+            
+        }
+    }
+}
